b071495d6b6fb92c6110aae05d45cdaea14a84ad
Jabi <michal.jabi@gmail.com>
2020-12-18 07:38:34 +0100

    chore: task-to-start-the-day

---
+++ src/exercises/009-task-to-start-the-day.ts
@@ -0,0 +1,24 @@
+/**
+
+    Zadanie na start:
+
+    #1
+    Przygotuj model danych dla operacji bankowej spełniający następujące wymagania:
+    > każda operacja posiada swój unikatowy identyfikator, tytuł oraz datę kiedy miała miejsce
+    > są dwa typy operacji: uznania i obciążenia -> in / out
+
+    #2
+    Pokaż kilka danych/wpisów wykorzystujących model - wymyśl te wpisy.
+
+    #3
+    Czy potrafisz pokazać te dane jako lista operacji?
+
+    #4*
+    Nastąpiła zmiana wymagań, istnieje 3 rodzaj operacji: anulowane -> cancelled.
+    Jednocześnie nie chcemy żeby poprzednie listy dotyczyła ta zmiana,
+    tylko nowe operacje mogą być anulowane.
+    Pokaż kolejną - nową listę (wymyśl wpisy) korzystającą z 3 rodzajów operacji,
+    przy jednoczesnym zachowaniu starego działania poprzedniej listy
+    (nieuwzględniającego tego 3 rodzaju)
+
+ */
